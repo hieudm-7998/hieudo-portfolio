af15aeaeb031e0b219ebc450dd3c225a666e8514
hieudm-smartosc <hieudm@smartosc.com>
2022-08-12 22:21:00 +0700

    COMMIT

--- layouts/projects.vue
+++ layouts/projects.vue
@@ -2,7 +2,7 @@
   <div class="app container">
     <Header />
     <div class="w-100 grid grid-cols-12 xl:grid-cols-5 xl:gap-4">
-      <ProjectsNavigation class="col-span-12 xl:col-span-0" />
+      <ProjectsNavigation class="col-span-12 xl:col-span-1" />
       <Nuxt class="col-span-12 xl:col-span-4" />
     </div>
   </div>
